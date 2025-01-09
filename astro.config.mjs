d9dbaf95aeaa839d826e85056a8f3218360ae99a
Dimitar Hristov <dhristov@ATD-US.com>
2025-01-09 12:50:55 +0200

    github actions + idea package

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://mitkosa333.github.io',
+    base: 'personal-blog',
     integrations: [
         mdx(),
         sitemap(),
